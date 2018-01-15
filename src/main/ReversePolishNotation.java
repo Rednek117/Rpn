70d947a91c8ba44500576b74555e7b1347bdd743
Rednek <roman.sharaev117@gmail.com>
2018-01-16 01:18:12 +0200

    Rpn transformation

---
+++ src/main/ReversePolishNotation.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.util.Stack;
+
+public class ReversePolishNotation {
+
+    public static void  check(String input){                                        // checking text grouped in [ ] does not appear in the input file.
+        String s = input.replaceAll("(\\]|\\[).*(\\]|\\[)", " ");
+        if(s != null)
+        execute(s);
+        else System.out.println("Enter algebraic expression");
+
+    }
+    public static void execute(String s){                                           // RPN transformation
+
+        char[] c = s.toCharArray();
+        Stack<Character> stack = new Stack<Character>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < c.length; i++)
+            switch (c[i]){
+                case '+':
+                case '-':
+                    while(!stack.empty() && (stack.peek().equals('*') || stack.peek().equals( '/'))){
+                        sb.append(' ').append(stack.pop());
+                    }
+                case '^':
+                case '*':
+                case '/':
+                    sb.append(' ');
+                case '(':
+                    stack.push(c[i]);
+                case ' ':
+                    break;
+                case ')':
+                    while (!stack.empty() && stack.peek() !='('){
+                        sb.append(' ').append(stack.pop());
+                    }
+                    if(!stack.isEmpty()) stack.pop();
+                    break;
+                default:
+                    sb.append(c[i]);
+                    break;
+            }
+            while (!stack.empty()) sb.append(' ').append(stack.pop());
+            System.out.println(sb.toString());
+    }
+}
--- src/main/Rpn.java
+++ src/main/Rpn.java
@@ -1,50 +1,11 @@
 package main;
 
-import java.util.Stack;
 
-public class Rpn {
+public class Rpn extends ReversePolishNotation {
     public static void main(String[] args) {
-        System.out.println(reversePolishNotation("(a+(b*c))\n" +
+        check("(a+(b*c))\n" +               // enter the expression
                 "((a+b)*(z+x))\n" +
-                "((a+t)*((b+(a+c))^(c+d)))"));
-    }
+                "((a+t)*((b+(a+c))^(c+d)))");
 
-
-    public static String reversePolishNotation(String input){
-        if(input == null) return new String("");
-        String s = input.replaceAll("(\\]|\\[).*(\\]|\\[)", " ");
-        char[] letters = s.toCharArray();
-        Stack<Character> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < letters.length; i++)
-            switch (letters[i]){
-                case '+':
-                case '-':
-                    while(!stack.empty() && (stack.peek().equals('*') || stack.peek().equals( '/'))){
-                        sb.append(' ').append(stack.pop());
-                    }
-                case '^':
-                case '*':
-                case '/':
-                    sb.append(' ');
-                case '(':
-                    stack.push(letters[i]);
-                case ' ':
-                    break;
-                case ')':
-                    while (!stack.empty() && stack.peek() !='('){
-                        sb.append(' ').append(stack.pop());
-                    }
-                    if(!stack.isEmpty()) stack.pop();
-                    break;
-                default:
-                     sb.append(letters[i]);
-                     break;
-            }
-            while (!stack.empty()) sb.append(' ').append(stack.pop());
-
-
-        return sb.toString();
     }
 }
